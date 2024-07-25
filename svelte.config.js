b56427d703ab7a975462569b8c4373c9b045dcbc
Babakar <bbd2501@gmail.com>
2024-07-25 12:06:47 +0000

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
+import adapter from '@sveltejs/adapter-cloudflare';
 
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	preprocess: vitePreprocess(),
-
-	resolve: {
-		alias: {
-		  '@': path.resolve('src')
-		}
-	  }
-
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
+	}
 };
-
-export default config;
 
